e629b987327914ad7b7f41244d61534eae035e6d
Rossbach Maxime <114393700+MaximeRsbch@users.noreply.github.com>
2024-05-02 14:50:18 +0200

    Add files via upload

---
+++ src/main/java/com/rossbach/MyAnimeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.rossbach;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyAnimeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyAnimeBackendApplication.class, args);
+	}
+
+}
